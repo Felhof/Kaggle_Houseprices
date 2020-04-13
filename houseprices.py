fe9a099cb7826e3a98802ac38fb34ac065f3651d
Felix <fh4417@ic.ac.uk>
2020-04-13 23:26:01 +0100

    write function to fit ann to data

--- houseprices.py
+++ houseprices.py
@@ -1,5 +1,6 @@
 from functools import reduce
 from itertools import chain
+from sklearn.model_selection import train_test_split
 
 import numpy as np
 import pandas as pd
@@ -12,19 +13,18 @@ class HouseNet(nn.Module):
     def __init__(self, input_, neurons, activations, out=1):
         super(HouseNet, self).__init__()
 
-        neurons.append(output)
 
         def activation(name):
             return {
-                "Id":None
-                "Sigmoid": nn.Sigmoid()
+                "Id":None,
+                "Sigmoid": nn.Sigmoid(),
                 "ReLU": nn.ReLU()
             }[name]
 
-        self._net = Sequential(
-            filter(None, chain(*list(
+        self._net = nn.Sequential(
+            *filter(None, chain(*list(
                 zip(
-                    [nn.Linear(inp, outp) for inp, out in zip([input_,*neurons],[*neurons,out])],
+                    [nn.Linear(inp, outp) for inp, outp in zip([input_,*neurons],[*neurons,out])],
                     [activation(a) for a in activations]
                     )
                 )))
@@ -33,19 +33,53 @@ class HouseNet(nn.Module):
     def forward(self, input_):
         return self._net.forward(input_)
 
+
+def fit_model(model, X, y):
+    epochs = 10
+    batch_size = 80
+    learning_rate = 0.001
+
+    loss_function = nn.MSELoss()
+    optimiser = torch.optim.Adam(list(model.parameters()), lr=learning_rate)
+
+    train_dataset = torch.utils.data.TensorDataset(torch.Tensor(np.array(X)), torch.Tensor(np.array(y)))
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size, shuffle=True)
+
+    print("fitting model")
+    for epoch in range(epochs):
+        print("epoch: {}".format(epoch))
+        for (x, ground_truth) in train_loader:
+            output = torch.squeeze(model(x))
+            loss = loss_function(output, ground_truth)
+
+            optimiser.zero_grad()
+            loss.backward()
+            optimiser.step()
+
+    print("done!")
+
+
+def evaluate(model, X, y):
+    pass
+
+
 if __name__ == "__main__":
 
     train_df=pd.read_csv("train.csv")
     evaluation_df=pd.read_csv("test.csv")
 
-    data_raw = pd.concat([train_df, evaluation_df], ignore_index=True, sort=False)
-    data_processed = preprocess(data_raw)
-    train_df, evaluation_df = np.split(data_processed, [len(train_df)])
-    evaluation_df.drop("SalePrice", axis="columns", inplace=True)
+    #data_raw = pd.concat([train_df, evaluation_df], ignore_index=True, sort=False)
+    #data_processed = preprocess(data_raw)
+    #train_df, evaluation_df = np.split(data_processed, [len(train_df)])
+    #evaluation_df.drop("SalePrice", axis="columns", inplace=True)
 
-    train_features = train_df.drop("SalePrice", axis="columns")
+    train_features = train_df[["YearBuilt","PoolArea","GrLivArea","GarageArea"]]
     train_target = train_df["SalePrice"]
 
     X_train, X_test, y_train, y_test = train_test_split(train_features, train_target)
     y_train = y_train.astype(int)
     y_test = y_test.astype(int)
+
+    model = HouseNet(4, [10,5],["Sigmoid","ReLU","Id"])
+    fit_model(model, X_train, y_train)
+    evaluate(model, X_test, y_test)
